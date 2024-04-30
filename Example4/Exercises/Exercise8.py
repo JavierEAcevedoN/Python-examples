e67ee0c8299bec52f3a5b0060926ee5e8ffa83d6
Javier Eduardo Acevedo Noguera <166556314+JavierEAcevedoN@users.noreply.github.com>
2024-04-29 22:05:42 -0500

    feat: se agrago un ejercicio

--- Example4/Exercises/Exercise8.py
+++ Example4/Exercises/Exercise8.py
@@ -1,1 +1,18 @@
 """Escribir un programa que pida al usuario una palabra y muestre por pantalla el número de veces que contiene cada vocal."""
+abc = "abcdefghijklmnopqrstuvwxyz"
+l_contadas = [] 
+
+def buspalabra(palabra, letra):
+    contador_l = 0
+    for i in palabra:
+        if i == letra:
+            contador_l += 1
+    return contador_l
+
+palabra = input("ingresa una palabra ")
+
+for i in abc:
+    l_contadas.append(buspalabra(palabra, i.lower()))
+
+for i in len(l_contadas):
+    print(f"se encontraron {l_contadas[i]} de la letra {abc[i]} en la palabra ingresada")
