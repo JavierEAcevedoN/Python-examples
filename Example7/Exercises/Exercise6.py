269b51d507599be4c80fcaed2a4605d25a03eab4
Javier Eduardo Acevedo Noguera <166556314+JavierEAcevedoN@users.noreply.github.com>
2024-05-05 03:18:40 +0200

    feat: terminando los ejercicios

--- Example7/Exercises/Exercise6.py
+++ Example7/Exercises/Exercise6.py
@@ -1,19 +1,29 @@
 """ Escribir un programa que gestione las facturas pendientes de cobro de una empresa. Las facturas se almacenarán en un diccionario donde la clave de cada factura será el número de factura y el valor el coste de la factura. El programa debe preguntar al usuario si quiere añadir una nueva factura, pagar una existente o terminar. Si desea añadir una nueva factura se preguntará por el número de factura y su coste y se añadirá al diccionario. Si se desea pagar una factura se preguntará por el número de factura y se eliminará del diccionario. Después de cada operación el programa debe mostrar por pantalla la cantidad cobrada hasta el momento y la cantidad pendiente de cobro. """
 
 facturas = {}
+choice = 1
+cobrado = 0
+pendiente = 0
 
 def agregarfactura(factura):
-    factura = dict
+    factura = dict(factura)
+    global pendiente
     numero = int(input("ingresa el numero de la factura "))
     precio = int(input("ingresa el precio de la factura "))
     factura.update({numero : precio})
+    pendiente += precio
+    return factura
 
 def eliminarfactura(factura):
-    factura = dict
+    factura = dict(factura)
+    global pendiente
+    global cobrado
     numero = int(input("ingresa el numero de la factura "))
-    factura.pop(numero, "no se encontro esa factura")
+    cobrado += factura[numero]
+    pendiente -= factura[numero]
+    factura.pop(numero)
+    return factura
 
-choice = 4
 
 while True:
     choice = int(input("0 para terminar, 1 para agregar una factura, 2 para pagar una factura "))
@@ -21,9 +31,10 @@ while True:
         print("terminando el proceso")
         break
     elif choice == 1:
-        agregarfactura(facturas)
+        facturas = agregarfactura(facturas)
     elif choice == 2:
-        eliminarfactura(facturas)
+        facturas = eliminarfactura(facturas)
     elif choice == 3:
         for i,j in facturas.items():
-            print(i, "=", j)
+            print(i, ":", j)
+    print(f"esta es la cantidad cobrada has ahora {cobrado} esta es la cantidad pendiente por cobrar {pendiente}")
--- Example7/Exercises/Exercise7.py
+++ Example7/Exercises/Exercise7.py
@@ -1,3 +1,76 @@
 """ Escribir un programa que permita gestionar la base de datos de clientes de una empresa. Los clientes se guardarán en un diccionario en el que la clave de cada cliente será su NIF, y el valor será otro diccionario con los datos del cliente (nombre, dirección, teléfono, correo, preferente), donde preferente tendrá el valor True si se trata de un cliente preferente. El programa debe preguntar al usuario por una opción del siguiente menú: (1) Añadir cliente, (2) Eliminar cliente, (3) Mostrar cliente, (4) Listar todos los clientes, (5) Listar clientes preferentes, (6) Terminar. En función de la opción elegida el programa tendrá que hacer lo siguiente:Preguntar los datos del cliente, crear un diccionario con los datos y añadirlo a la base de datos.Preguntar por el NIF del cliente y eliminar sus datos de la base de datos.Preguntar por el NIF del cliente y mostrar sus datos.Mostrar lista de todos los clientes de la base datos con su NIF y nombre.Mostrar la lista de clientes preferentes de la base de datos con su NIF y nombre.Terminar el programa. """
 
-informacion = {}
+clientes = {}
+
+def agregarcliente(cliente):
+    cliente = dict(cliente)
+    nif = input("ingresa el NIF del cliente ")
+
+    nombre = input("ingresa el nombre del cliente ")
+
+    dirección = input("ingresa la dirección del cliente ")
+
+    teléfono = input("ingresa el teléfono del cliente ")
+
+    correo = input("ingresa el correo del cliente ")
+
+    preferente = input("el cliente es preferente? (si o no) ")
+    preferente = True if preferente.lower() == "si" else False
+
+    if nif == "":
+        print("ingresa un NIF")
+        return cliente
+    else:
+        cliente.update(
+            {nif:{
+                "Nombre":nombre,
+                "Dirección":dirección,
+                "Teléfono":teléfono,
+                "Correo":correo,
+                "Preferente":preferente
+            }})
+        return cliente
+
+def eliminarcliente(cliente):
+    cliente = dict(cliente)
+    nif = input("ingresa el nif del cliente a eliminar ")
+    cliente.pop(nif,None)
+    return cliente
+
+def mostrarcliente(cliente):
+    cliente = dict(cliente)
+    nif = input("ingresa el nif del cliente para ver la informacion ")
+    print(f"NIF {nif}:")
+    for i,j in cliente[nif].items():
+        print(f"{i} = {j}")
+    return cliente
+
+def listartodosclientes(cliente):
+    cliente = dict(cliente)
+    for i in cliente:
+            print(f"NIF: {i} Nombre: {cliente[i]["Nombre"]}")
+
+def listarclientespreferentes(cliente):
+    cliente = dict(cliente)
+    for i in cliente:
+        if cliente[i]["Preferente"] == True:
+            print(f"NIF: {i} Nombre: {cliente[i]["Nombre"]}")
+    return cliente
+
+choice = 1
+
+while True:
+    choice = int(input("ingresa la opcion (1) Añadir cliente, (2) Eliminar cliente, (3) Mostrar cliente, (4) Listar todos los clientes, (5) Listar clientes preferentes, (6) Terminar. "))
+    if choice == 1:
+        clientes = agregarcliente(clientes)
+    elif choice == 2:
+        clientes = eliminarcliente(clientes)
+    elif choice == 3:
+        mostrarcliente(clientes)
+    elif choice == 4:
+        listartodosclientes(clientes)
+    elif choice == 5:
+        listarclientespreferentes(clientes)
+    elif choice == 6:
+        print("terminando proceso")
+        break
